182f85ceedaeb99a1865de90d5216ffc9fc71e57
FlowMeta <collerdesignz@gmail.com>
2026-08-17 04:41:55 +0200

    Refactor server query handling and add server list

--- check_server.py
+++ check_server.py
@@ -3,33 +3,58 @@ import a2s
 
 app = Flask(__name__)
 
-IP = "95.135.1.25"
-PORT = 9877
+SERVERS = [
+    {
+        "id": "hengoria",
+        "ip": "95.135.1.25",
+        "port": 9877
+    },
+
+    # Add more servers here later
+    # {
+    #     "id": "another-server",
+    #     "ip": "123.123.123.123",
+    #     "port": 9877
+    # }
+]
 
 
-@app.route("/server")
-def get_server():
+def query_server(server):
     try:
-        info = a2s.info((IP, PORT))
+        info = a2s.info((server["ip"], server["port"]))
 
-        return jsonify({
+        return {
+            "id": server["id"],
             "online": True,
             "server": info.server_name,
             "players": info.player_count,
             "max_players": info.max_players,
             "map": info.map_name
-        })
+        }
 
     except Exception as e:
-        return jsonify({
+        return {
+            "id": server["id"],
             "online": False,
+            "players": 0,
+            "max_players": 0,
             "error": str(e)
-        })
+        }
 
 
 @app.route("/")
 def home():
     return "V Rising Server API is running!"
+
+
+@app.route("/servers")
+def get_servers():
+    results = []
+
+    for server in SERVERS:
+        results.append(query_server(server))
+
+    return jsonify(results)
 
 
 if __name__ == "__main__":
